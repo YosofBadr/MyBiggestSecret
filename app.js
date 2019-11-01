13d68e617b2177474297034be598932bffa03ae5
Yosof Badr <yosof_badr@hotmail.com>
2019-11-01 20:08:01 +0000

    Set up mongo database using mongoose

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Test application for Passport.js user authentication - following Colt Steele
 
 var express = require('express');
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/user_auth_demo", { useNewUrlParser: true, useUnifiedTopology: true }); 
 
 var app = express();
 app.set("view engine", "ejs");
